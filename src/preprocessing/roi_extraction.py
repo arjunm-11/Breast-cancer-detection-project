f3f254c14db1d85611ef0c968f8a1d3b6a3fafad
Arjun <148117217+arjunm-11@users.noreply.github.com>
2025-10-28 18:53:54 +0530

    Delete src directory

--- src/preprocessing/roi_extraction.py
+++
@@ -1,7 +0,0 @@
-import cv2, os
-
-def crop_roi(img_path, roi_coords, save_path):
-    img = cv2.imread(img_path, 0)
-    x, y, w, h = roi_coords
-    roi = img[y:y+h, x:x+w]
-    cv2.imwrite(save_path, roi)
