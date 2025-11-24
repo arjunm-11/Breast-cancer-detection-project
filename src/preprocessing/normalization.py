9db7f1c9c43a45b8ef638a91595fc2cad5e2c1cb
Arjun <148117217+arjunm-11@users.noreply.github.com>
2025-11-25 01:04:41 +0530

    Delete src directory

--- src/preprocessing/normalization.py
+++
@@ -1,6 +0,0 @@
-import cv2, os
-
-def normalize_img(img_path, save_path):
-    img = cv2.imread(img_path, 0)
-    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
-    cv2.imwrite(save_path, img)
--- src/utils/augmentation.py
+++
@@ -1,5 +0,0 @@
-from keras.preprocessing.image import ImageDataGenerator
-
-def get_augmented_generator(path, batch_size=32):
-    datagen = ImageDataGenerator(rescale=1./255, rotation_range=15, zoom_range=0.1, horizontal_flip=True, fill_mode='nearest')
-    return datagen.flow_from_directory(path, target_size=(128,128), color_mode='grayscale', class_mode='categorical', batch_size=batch_size)
